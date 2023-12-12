790854cf8c6f3cf27c9ef00f7372f49aa4174c48
Ruslan Khissamov <ruslan.khissamov@meteorhr.com>
2023-12-12 19:44:56 +0600

    commit

--- jwt/jwt.js
+++ jwt/jwt.js
@@ -18,9 +18,9 @@ const RefreshTokenUpdate = async (req, res, next) => {
     const refreshToken = cookies.refreshToken;
     const deviceId = headers[HEADER_DEVICE_ID];
 
-    console.info('Device ID Headers: ' + deviceId);
-    console.info('Refresh Token Cookies: ' + refreshToken);
-    console.info('Find refresh token');
+    //console.info('Device ID Headers: ' + deviceId);
+    //console.info('Refresh Token Cookies: ' + refreshToken);
+    //console.info('Find refresh token');
 
     const findAndUpdate = await findRefreshTokenAndUpdated(refreshToken, deviceId);
 
@@ -135,14 +135,15 @@ export default () => {
       const {headers, cookies} = req;
       // Получения refreshToken
       const refreshToken = cookies.refreshToken;
+      console.log(cookies);
       console.log("Refresh Token: " + refreshToken);
       // Если отсутствует Device ID в запросе, обнулить куки, поставить статус ответа 401, направить сообще об ошибки в формате JSON
       if(!headers[HEADER_DEVICE_ID]){
-        return setCookieAndSendErrorMessage(res, 'Headers not found device id');
+        return res.send(setCookieAndSendErrorMessage(res, 'Headers not found device id'));
       }
       // Если отсутствует Refresh Token в запросе, обнулить куки, поставить статус ответа 401, направить сообще об ошибки в формате JSON
       if(!refreshToken){
-        return setCookieAndSendErrorMessage(res, 'Refresh Token not found');
+        return res.send(setCookieAndSendErrorMessage(res, 'Refresh Token not found'));
       } 
 
       // Если существует Access Token
@@ -164,11 +165,11 @@ export default () => {
                 return res.send(handleServerError(res, error));
               }
           } else {
-            console.log('Update refresh token, expiration time access token');
+            // console.log('Update refresh token, expiration time access token');
             return await RefreshTokenUpdate(req, res, next);
           }
       } else {
-          console.log('Update refresh token, access token not found');
+          // console.log('Update refresh token, access token not found');
           return await RefreshTokenUpdate(req, res, next);
       }  
     } catch(error) {
